ca704aa50a168b25735cf99faaae6200f9da0cb0
SmejoTT <adrian.trajlinek@gmail.com>
2020-04-26 19:34:39 +0100

    setup grid

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+const setUpGrid = function () {
+    const gameScreen = document.querySelector('.grid');
+    const divsOfButtons = gameScreen.children;
+    const positions =
+        [3,4,5,2,3,4,5,6,1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,2,3,4,5,6,3,4,5];
+
+    for (i=0; i<divsOfButtons.length;i++){
+        let btn = document.getElementById(''+(i+1));
+        if (i===2 || i===7 || i===14 || i===21 || i===28 || i===33 || i===36){
+            btn.style.border ='black solid 1px';
+        } else {
+            btn.style.border ='solid black 1px';
+            btn.style.borderRight  = 'none';
+        }
+        if (!(i===22 || i===28 ||i===29 || i===33 || i===34 || i===35 || i===36)){
+            btn.style.borderBottom = 'none';
+        }
+
+        if (i<3) {
+            divsOfButtons[i].style.gridRow = '1';
+        }
+        if (i>2 && i<8){
+            divsOfButtons[i].style.gridRow = '2';
+        }
+        if (i>7 && i<15){
+            divsOfButtons[i].style.gridRow = '3';
+        }
+        if (i>14 && i<22){
+            divsOfButtons[i].style.gridRow = '4';
+        }
+        if (i>21 && i<29){
+            divsOfButtons[i].style.gridRow = '5';
+        }
+        if (i>28 && i<34){
+            divsOfButtons[i].style.gridRow = '6';
+        }
+        if (i>33 && i<37){
+            divsOfButtons[i].style.gridRow = '7';
+        }
+        divsOfButtons[i].style.gridColumn = positions[i].toString();
+    }
+}
 const game = function () {
     let numberOfPieces = 36;
     const startGame = function () {
@@ -8,36 +50,9 @@ const game = function () {
         playBtn.addEventListener('click', () =>{
             introScreen.classList.add('fadeOut');
             gameScreen.classList.add('fadeIn');
-            var buttons = gameScreen.children;
-            var positions =
-                [3,4,5,2,3,4,5,6,1,2,3,4,5,6,7,1,2,3,4,5,6,7,1,2,3,4,5,6,7,2,3,4,5,6,3,4,5]
-            for (i=0; i<buttons.length;i++){
-                if (i<3) {
-                    buttons[i].style.gridRow = '1';
-                }
-                if (i>2 && i<8){
-                    buttons[i].style.gridRow = '2';
-                }
-                if (i>7 && i<15){
-                    buttons[i].style.gridRow = '3';
-                }
-                if (i>14 && i<22){
-                    buttons[i].style.gridRow = '4';
-                }
-                if (i>21 && i<29){
-                    buttons[i].style.gridRow = '5';
-                }
-                if (i>28 && i<34){
-                    buttons[i].style.gridRow = '6';
-                }
-                if (i>33 && i<37){
-                    buttons[i].style.gridRow = '7';
-                }
-                buttons[i].style.gridColumn = positions[i].toString();
-            }
         });
     };
     startGame();
 }
-
+setUpGrid();
 game();
